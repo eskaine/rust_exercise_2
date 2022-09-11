b52efce2c31b8dd5e769f7717b26bb39cfe607f2
eskaine <eskaineliu@gmail.com>
2022-09-12 00:45:09 +0800

    added structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+enum PaymentType {
+    DigitalToken,
+    Cash,
+}
+
+struct Seller {
+    payment_type: PaymentType,
+    price: f32,
+    balance: f32,
+}
+
+struct Buyer {
+    name: String,
+    payment_type: PaymentType,
+    balance: f32,
+}
+
+struct BuyerGroup {
+    members: Vec<Buyer>,
+}
+
+impl BuyerGroup {
+    fn add_member(&mut self, member: Buyer) {
+        self.members.push(member);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let buyer_1 = Buyer {
+        name: "John".to_owned(),
+        payment_type: PaymentType::DigitalToken,
+        balance: 100.00,
+    };
+
+    let mut buyers = BuyerGroup {
+        members: Vec::new(),
+    };
+
+    buyers.add_member(buyer_1);
 }

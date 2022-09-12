43a03538a670ec0faef9e1e09d5be36dacd13950
eskaine <eskaineliu@gmail.com>
2022-09-12 09:10:07 +0800

    finish assignment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, Eq)]
 enum PaymentType {
     DigitalToken,
     Cash,
@@ -23,6 +24,28 @@ impl BuyerGroup {
     fn add_member(&mut self, member: Buyer) {
         self.members.push(member);
     }
+
+    fn find_buyer(&self, payment_type: PaymentType) -> i32 {
+        for (i, buyer) in self.members.iter().enumerate() {
+            if buyer.payment_type == payment_type {
+                return i as i32;
+            }
+        }
+
+        return -1;
+    }
+
+    fn buy(&mut self, buyer_index: i32, seller: &mut Seller) {
+        let mut buyer = &mut self.members[buyer_index as usize];
+
+        while buyer.balance >= seller.price {
+            buyer.balance -= seller.price;
+            seller.balance += seller.price;
+
+            println!("Seller Balance: {}", seller.balance);
+            println!("Buyer Balance: {}", buyer.balance);
+        }
+    }
 }
 
 fn main() {
@@ -32,9 +55,25 @@ fn main() {
         balance: 100.00,
     };
 
+    let buyer_2 = Buyer {
+        name: "Sally".to_owned(),
+        payment_type: PaymentType::Cash,
+        balance: 100.00,
+    };
+
     let mut buyers = BuyerGroup {
         members: Vec::new(),
     };
 
     buyers.add_member(buyer_1);
+    buyers.add_member(buyer_2);
+
+    let mut seller = Seller {
+        payment_type: PaymentType::Cash,
+        price: 10.0,
+        balance: 0.0,
+    };
+
+    let index = buyers.find_buyer(PaymentType::Cash);
+    buyers.buy(index, &mut seller);
 }
